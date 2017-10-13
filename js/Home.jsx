b14907e53274e6da3e279251fb4af75db06fa5ab
Masato Seki <masatoseki@the-neighbourhood.com>
2017-10-13 08:19:54 +0100

    Map twice to get data

--- js/Home.jsx
+++ js/Home.jsx
@@ -5,25 +5,37 @@ import axios from 'axios';
 
 class Home extends Component {
   state = {
-    id: 'This is ID'
+    posts: [],
+    page: 1,
   };
   componentDidMount() {
     const apiKey = 'knXlIfwA1H5FNsYFz28ZArIt5UMWSoNVu3ky7Uk0Y50hPnw0pY';
     const blogTitle = 'homeInsprd';
-    axios.get(`http://api.tumblr.com/v2/blog/${blogTitle}.tumblr.com/posts?&offset=21&api_key=${apiKey}`)
+    const number = 0;
+    axios.get(`http://api.tumblr.com/v2/blog/${blogTitle}.tumblr.com/posts?&offset=${number}&limit=2&api_key=${apiKey}`)
       .then((response) => {
-        const posts = response.data.response.posts;
-        for (let i = 0; i < posts.length; i += 1) {
-          console.log(posts[i].id);
-          this.setState({ id: posts[i].id });
-        }
-      });
+        const posts = response.data.response.posts.map(obj => obj);
+        console.log(response.data.response.posts);
+        this.setState({ posts });
+    });
   }
+
+  handleClick = () => {
+    console.log(this.state.page += 20);
+  }
+
   render() {
     return (
       <section className="home">
         <div className="wrapper">
-          <h1>{this.state.id}</h1>
+          <ul className="postList">
+            {this.state.posts.map(post =>
+              <img src={post.photos[0].original_size.url} alt={post.id} key={post.id} />
+            )}
+          </ul>
+          <button onClick={this.handleClick}>
+            Load more
+          </button>
         </div>
       </section>
     );
