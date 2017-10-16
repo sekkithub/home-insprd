177625dea3fc85a73b95de79737d22f4b76a018b
Masato Seki <masatoseki@the-neighbourhood.com>
2017-10-16 21:30:50 +0100

    All in home component

--- js/Home.jsx
+++ js/Home.jsx
@@ -1,25 +1,51 @@
 // @flow
 
 import React, { Component } from 'react';
-// import { string } from 'prop-types';
-import Posts from './Posts';
+import axios from 'axios';
+
+const apiKey = 'knXlIfwA1H5FNsYFz28ZArIt5UMWSoNVu3ky7Uk0Y50hPnw0pY';
+const blogApi = 'http://api.tumblr.com/v2/blog/homeinsprd.tumblr.com';
+const loadingPostsAtOnce = 2;
 
 class Home extends Component {
   state = {
+    selectedTag: '',
     tags: [
       'mirror',
       'plant',
       'bathroom',
       'toilet',
     ],
-    selectedTag: 'asd',
+    posts: [],
+    firstLoadingEntry: 0,
   }
 
-  handleClick = () => {
-    this.setState({selectedTag: 'bathroom'}, () => {
-      console.log(this.state.selectedTag);
+  componentDidMount() {
+    const firstLoadingEntry = this.state.firstLoadingEntry;
+    axios.get(`${blogApi}/posts?&offset=${firstLoadingEntry}&limit=${loadingPostsAtOnce}&api_key=${apiKey}&tag=${this.state.selectedTag}`)
+      .then((response) => {
+        const posts = response.data.response.posts.map(obj => obj);
+        this.setState({ posts });
+    });
+  }
+
+  runFilter = (tag: string) => {
+    this.setState({ posts: [], firstLoadingEntry: 0, selectedTag: tag }, () => {
+      axios.get(`${blogApi}/posts?&offset=${this.state.firstLoadingEntry}&limit=${loadingPostsAtOnce}&api_key=${apiKey}&tag=${this.state.selectedTag}`)
+        .then((response) => {
+          const posts = response.data.response.posts.map(obj => obj);
+          this.setState({ posts });
+      });
     });
   };
+
+  loadMore = () => {
+    axios.get(`${blogApi}/posts?&offset=${this.state.firstLoadingEntry += loadingPostsAtOnce}&limit=${loadingPostsAtOnce}&api_key=${apiKey}&tag=${this.state.selectedTag}`)
+      .then((response) => {
+        const posts = response.data.response.posts.map(obj => obj);
+        this.setState({posts: this.state.posts.concat(posts)});
+    });
+  }
 
   render() {
     return (
@@ -29,21 +55,25 @@ class Home extends Component {
           <ul className="tags">
             {this.state.tags
               .map(tag => (
-                // <li role="presentation" key={tag} onClick={(e) => this.handleClick(tag, e)}>{tag}</li>
-                <li role="presentation" key={tag} onClick={this.handleClick}>{tag}</li>
+                <li role="presentation" key={tag} onClick={() => this.runFilter(tag)}>{tag}</li>
               )
             )}
           </ul>
 
-          <Posts />
+          <ul className="posts">
+            {this.state.posts
+              .filter(post => `${post.tags}`.indexOf(post.tags) >= 0)
+              .map(post => (
+                <img src={post.photos[0].original_size.url} key={post.id} alt={post.id} />
+              )
+            )}
+          </ul>
+
+          <button onClick={this.loadMore}>Load more</button>
         </div>
       </section>
     )
   }
 }
-
-// Home.propTypes = {
-//   tag: string.isRequired
-// };
 
 export default Home;
